8d4e03a249eaa4880077f9fb014b8d7f8eb9dffb
Jakov Vodanović <138793029+Jakov123123@users.noreply.github.com>
2025-11-16 11:23:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title("🎮 Video Game Sales Analytics")
 
 @st.cache_resource
 def get_connection():
-    return duckdb.connect("C:/Users/jakov/OneDrive/Desktop/video_games.db", read_only=True)
+    return duckdb.connect("video_games.db", read_only=True)
 
 conn = get_connection()
 
@@ -16,4 +16,5 @@ total_query = "SELECT ROUND(SUM(Global_Sales), 2) as total_sales FROM fact_sales
 total = conn.execute(total_query).fetchdf()
 
 st.header("Total Global Sales")
+
 st.metric("Total Sales", f"${total['total_sales'][0]:,.1f}M")
